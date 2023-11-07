4d8c884f92221a2b590fdcf9270a2b1cf95cf0d9
amokfa <iaansagar@gmail.com>
2023-11-07 20:29:50 +0530

    header and footer and config context

--- static/js/constexpr/renderer.js
+++ static/js/constexpr/renderer.js
@@ -9,9 +9,24 @@ let PageResources
 let NewBody
 
 async function fetchResources() {
-    return {
+    let res = {
         mainCss: await fetch('/static/css/styles.css').then(res => res.text()),
+        cfg: await fetch('/collections/config.json')
+            .then(res => res.json()),
+        posts: await fetch('/collections/posts.json')
+            .then(res => res.json()),
+        nav: await fetch('/collections/nav.json')
+            .then(res => res.json()),
+        projects: await fetch('/collections/projects.json')
+            .then(res => res.json())
+    };
+    res.thisPost = res.posts.filter(p => p.url === document.location.pathname)[0]
+    if (res.thisPost) {
+        res.title = res.thisPost.title
+    } else {
+        res.title = ''
     }
+    return res
 }
 
 async function site_global_rendering() {
@@ -29,7 +44,7 @@ async function renderBody() {
     // document.body = document.createElement('body')
     NewBody = document.createElement('body')
     NewBody.setAttribute('render_date', `${new Date()}`)
-    NewBody.classList.add('light')
+    NewBody.classList.add('dark')
 
     ReactDOM.createRoot(NewBody)
         .render(e(
@@ -50,7 +65,7 @@ function Page() {
         React.Fragment,
         {},
         e(
-            PageResources.Consumer, null,
+            PageResources.Consumer, {},
             context => e('style', {}, context.mainCss)
         ),
         e('img', {src: '/static/img/bg.webp', className: 'bg', id: 'main_bg', loading: 'lazy'}),
@@ -70,20 +85,20 @@ function ViewBgBtn() {
 }
 
 function LeftSidebar() {
-    let marks
-    if (document.querySelector('article').children[0].tagName === 'H2') {
-        marks = []
-    } else {
-        marks = ['Top']
-    }
+    let marks = []
+    try {
+        if (document.querySelector('article').children[0].tagName !== 'H2') {
+            marks = ['Top']
+        }
+    } catch (e) {}
     marks.push(...Array.from(document.querySelectorAll('article h2'))
         .map(el => el.textContent))
     return e(
         'div',
-        {id: 'left-sidebar', style: {display: marks.length === 1 ? 'none' : null}},
+        {id: 'left-sidebar'},
         e(
             'div',
-            {className: 'dialog'},
+            {className: 'dialog', style: {display: marks.length === 1 ? 'none' : null}},
             e(
                 'div',
                 {className: 'heading'},
@@ -159,12 +174,65 @@ function RightSidebar() {
 
 function PageContent() {
     return e(
-        'div',
-        {id: 'body_wrapper'},
-        e(
-            'header', {}
-        ),
-        e('article', {}),
-        e('footer', {})
+        PageResources.Consumer,
+        {},
+        context => e(
+            'div',
+            {id: 'body_wrapper'},
+            e(
+                'header', {},
+                e(
+                    'nav', {},
+                    context.nav
+                        .map(item => e(
+                            'a', {href: item.href},
+                            item.name
+                        ))
+                ),
+                e('h1', {id: 'main_title'}, context.thisPost ? context.thisPost.title : null),
+            ),
+            e('article', {}),
+            e(
+                'footer', {},
+                e(
+                    'div', {className: 'links'},
+                    e(
+                        'div', {className: 'social'},
+                        e(
+                            'div', {className: 'footer-title'},
+                            'Share this page'
+                        ),
+                        e('a', {
+                            className: 'svg-icon twitter',
+                            rel: 'noopener',
+                            title: 'twitter',
+                            target: '_blank',
+                            href: `https://twitter.com/share?text=${context.title}&url=${context.cfg.url + window.location.pathname}`
+                        }),
+                        e('a', {
+                            className: 'svg-icon facebook',
+                            rel: 'noopener',
+                            title: 'facebook',
+                            target: '_blank',
+                            href: `https://www.facebook.com/sharer.php?u=${context.cfg.url + window.location.pathname}`
+                        }),
+                        e('a', {
+                            className: 'svg-icon reddit',
+                            rel: 'noopener',
+                            title: 'reddit',
+                            target: '_blank',
+                            href: `https://www.reddit.com/submit?title=${context.title}&url=${context.cfg.url + window.location.pathname}`
+                        }),
+                        e('a', {
+                            className: 'svg-icon hn',
+                            rel: 'noopener',
+                            title: 'hacker news',
+                            target: '_blank',
+                            href: `https://news.ycombinator.com/submitlink?t=${context.title}&u=${context.cfg.url + window.location.pathname}`
+                        }),
+                    )
+                )
+            )
+        )
     )
 }
