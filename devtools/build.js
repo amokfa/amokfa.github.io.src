ad27f864a3ad5c62f8a691f026f37795541ed8aa
amokfa <iaansagar@gmail.com>
2023-11-12 12:51:58 +0530

    nav bugfix

--- devtools/build.js
+++ devtools/build.js
@@ -26,7 +26,7 @@ async function main() {
 
     await execSh('npx sass ./static/scss/styles.scss:./static/css/styles.css --style compressed')
     await execSh(
-        `constexprjs ${process.argv.slice(2).join(' ')} --input=. --output=_out --entry /index.html --jobcount 12 --depfile devtools/deps.json --verbose`,
+        `constexprjs ${process.argv.slice(2).join(' ')} --input=. --output=_out --entry /index.html --jobcount 12 --depfile devtools/deps.json --literal-tag style --literal-tag prog`,
         {},
     )
 }
