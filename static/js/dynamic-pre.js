6642c75b76edd5cd0b851d7a65ea805cab475297
amokfa <iaansagar@gmail.com>
2023-12-06 19:04:59 +0530

    make dark theme default

--- static/js/dynamic-pre.js
+++ static/js/dynamic-pre.js
@@ -1,10 +1,10 @@
 function theme_initialize() {
-    if (localStorage.getItem('theme') === 'dark') {
-        document.body.classList.add('dark')
-        document.body.classList.remove('light')
-    } else {
+    if (localStorage.getItem('theme') === 'light') {
         document.body.classList.add('light')
         document.body.classList.remove('dark')
+    } else {
+        document.body.classList.add('dark')
+        document.body.classList.remove('light')
     }
 }
 theme_initialize()
